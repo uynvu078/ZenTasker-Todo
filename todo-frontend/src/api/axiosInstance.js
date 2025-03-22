527ed6fdabc78207e51f88e14b47ac9353098b77
uynvu078 <100883567+uynvu078@users.noreply.github.com>
2025-03-22 00:25:58 -0500

    Switch for GitHub Pages

--- todo-frontend/src/api/axiosInstance.js
+++ todo-frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://zentasker-4ear.onrender.com/api";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
